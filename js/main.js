3e57da0f70de49327bdb803b0c7787881e6323f3
wetavares <wetavares@gmail.com>
2021-03-15 22:09:02 -0300

    Commit finalizado aula javascrip

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
 
 
+function redirecionar(){
+    window.open("https://globallab.org/en/#.YE_5m9xv_IU");
+    //window.location.href = "https://globallab.org/en/#.YE_5m9xv_IU";
+}
+
 function clicou(){
-    document.getElementById("agradecimento");
-    console.log(document.getElementById("agradecimento"));
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar!<?b>";
+    //document.getElementById("agradecimento").innerHTML = "Obrigado por clicar!";
+    //console.log(document.getElementById("agradecimento"));
     //alert("Obrigado por Clicar!");
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse";
+   // document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("Trocar texto");
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Passe o mouse aqui!";
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui!";
+}
+
+function load(){
+    alert("Pagina Carregada.")
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
 }
 
 /*function soma(n1,n2){
